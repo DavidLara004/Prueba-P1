d7ba3f02bd361d985b86551a7bb97238ce953704
David Lara <david.lara.moreira@udla.edu.ec>
2023-05-03 15:16:19 -0500

    Primera parte del programa con documentacion

---
+++ Examenpuno.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Ingrese un numero entero positivo:");
+    scanf("%d", &n);
+    if (n>=0)
+    {
+        
+    }
+    else
+    {
+    printf("El numero ingresado no cumple con las condiciones.");
+    }
+    
+    return 0;
+}
